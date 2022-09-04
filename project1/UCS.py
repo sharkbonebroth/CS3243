264f829cfa95e2cedde057e432c04caa8e23f628
sharkbonebroth <chinhao61@gmail.com>
2022-09-04 23:44:33 +0800

    initial

---
+++ project1/UCS.py
@@ -0,0 +1,91 @@
+import sys
+
+# Helper functions to aid in your implementation. Can edit/remove
+#############################################################################
+######## Piece
+#############################################################################
+class Piece:
+    pass
+
+#############################################################################
+######## Board
+#############################################################################
+class Board:
+    pass
+
+#############################################################################
+######## State
+#############################################################################
+class State:
+    pass
+
+#############################################################################
+######## Implement Search Algorithm
+#############################################################################
+def search(rows, cols, grid, enemy_pieces, own_pieces, goals):
+    pass
+
+
+#############################################################################
+######## Parser function and helper functions
+#############################################################################
+### DO NOT EDIT/REMOVE THE FUNCTION BELOW###
+# Return number of rows, cols, grid containing obstacles and step costs of coordinates, enemy pieces, own piece, and goal positions
+def parse(testcase):
+    handle = open(testcase, "r")
+
+    get_par = lambda x: x.split(":")[1]
+    rows = int(get_par(handle.readline())) # Integer
+    cols = int(get_par(handle.readline())) # Integer
+    grid = [[1 for j in range(cols)] for i in range(rows)] # Dictionary, label empty spaces as 1 (Default Step Cost)
+    enemy_pieces = [] # List
+    own_pieces = [] # List
+    goals = [] # List
+
+    handle.readline()  # Ignore number of obstacles
+    for ch_coord in get_par(handle.readline()).split():  # Init obstacles
+        r, c = from_chess_coord(ch_coord)
+        grid[r][c] = -1 # Label Obstacle as -1
+
+    handle.readline()  # Ignore Step Cost header
+    line = handle.readline()
+    while line.startswith("["):
+        line = line[1:-2].split(",")
+        r, c = from_chess_coord(line[0])
+        grid[r][c] = int(line[1]) if grid[r][c] == 1 else grid[r][c] #Reinitialize step cost for coordinates with different costs
+        line = handle.readline()
+    
+    line = handle.readline() # Read Enemy Position
+    while line.startswith("["):
+        line = line[1:-2]
+        piece = add_piece(line)
+        enemy_pieces.append(piece)
+        line = handle.readline()
+
+    # Read Own King Position
+    line = handle.readline()[1:-2]
+    piece = add_piece(line)
+    own_pieces.append(piece)
+
+    # Read Goal Positions
+    for ch_coord in get_par(handle.readline()).split():
+        r, c = from_chess_coord(ch_coord)
+        goals.append((r, c))
+    
+    return rows, cols, grid, enemy_pieces, own_pieces, goals
+
+def add_piece( comma_seperated) -> Piece:
+    piece, ch_coord = comma_seperated.split(",")
+    r, c = from_chess_coord(ch_coord)
+    return [piece, (r,c)]
+
+def from_chess_coord( ch_coord):
+    return (int(ch_coord[1:]), ord(ch_coord[0]) - 97)
+
+### DO NOT EDIT/REMOVE THE FUNCTION HEADER BELOW###
+# To return: List of moves and nodes explored
+def run_UCS():
+    testcase = sys.argv[1]
+    rows, cols, grid, enemy_pieces, own_pieces, goals = parse(testcase)
+    moves, pathcost = search(rows, cols, grid, enemy_pieces, own_pieces, goals)
+    return moves, pathcost
